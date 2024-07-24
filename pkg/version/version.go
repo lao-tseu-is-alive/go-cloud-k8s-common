1abf90a97f8f3ff2a97d6e3a74bccab1e7edf0ae
cgil <lao.tseu.is.alive@gmail.com>
2024-07-24 11:00:48 +0200

    initial commit

---
+++ pkg/version/version.go
@@ -0,0 +1,10 @@
+package version
+
+var (
+	APP        = "goCloudK8sCommon"
+	AppSnake   = "go-cloud-k8s-common"
+	VERSION    = "0.1.0"
+	REPOSITORY = "https://github.com/lao-tseu-is-alive/go-cloud-k8s-common"
+	REVISION   = "unknown"
+	BuildStamp = "unknown"
+)
