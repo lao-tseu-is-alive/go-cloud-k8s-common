c6bab333898a5e9aeaad0129784feeff95c52555
cgil <lao.tseu.is.alive@gmail.com>
2024-08-05 09:46:15 +0200

    added Sec-Websocket-Protocol

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,7 +3,7 @@ package version
 var (
 	APP        = "goCloudK8sCommon"
 	AppSnake   = "go-cloud-k8s-common"
-	VERSION    = "0.3.3"
+	VERSION    = "0.3.4"
 	REPOSITORY = "https://github.com/lao-tseu-is-alive/go-cloud-k8s-common"
 	REVISION   = "unknown"
 	BuildStamp = "unknown"
