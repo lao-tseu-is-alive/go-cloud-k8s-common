782e828b658cf09665014056bb89619da2446cda
cgil <lao.tseu.is.alive@gmail.com>
2025-08-27 11:26:03 +0200

    migrated go version to mitigate CVE-2025-47907

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,7 +3,7 @@ package version
 var (
 	APP        = "goCloudK8sCommon"
 	AppSnake   = "go-cloud-k8s-common"
-	VERSION    = "0.5.2"
+	VERSION    = "0.5.3"
 	REPOSITORY = "https://github.com/lao-tseu-is-alive/go-cloud-k8s-common"
 	REVISION   = "" // Will be set to a valid git revision at compile time
 	BuildStamp = "" // Will be set to a valid timestamp at compile time
